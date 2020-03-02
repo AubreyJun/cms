93a94c2e3a683964029f4916769ee8aaa80d0361
aubrey <zhujun@ranko.cn>
2020-03-02 16:17:31 +0800

    Signed-off-by: aubrey <zhujun@ranko.cn>

--- config/dev/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=rm-bp1q4awtgmaz83249ko.mysql.rds.aliyuncs.com:3306;dbname=dev.cms',
-    'username' => 'ranko_dev',
-    'password' => 'sfs32e32&%$JHg',
-    'charset' => 'utf8'
-];
