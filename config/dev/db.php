8fa8e78e14affbb8f6326747c9585a38cff043c8
aubrey <zhujun@ranko.cn>
2020-03-06 22:02:56 +0800

    商业版本本地开发。不需要文件忽略 Signed-off-by: aubrey <zhujun@ranko.cn>

---
+++ config/dev/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=rm-bp1q4awtgmaz83249ko.mysql.rds.aliyuncs.com;dbname=dev.cms',
+    'username' => 'ranko_dev',
+    'password' => 'sdsdfds2323&((32)',
+    'charset' => 'utf8'
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
---
+++ config/dev/params.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'demo'=>'true',
+    'themeids'=>array(
+        'dev.biz.cms.cn'=>'basic'
+    )
+];
