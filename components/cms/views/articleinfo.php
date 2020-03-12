866d03906fd1d3a53641859775cffb7e872c8d44
aubrey <zhujun@ranko.cn>
2020-03-12 16:11:43 +0800

    设置文章内容页面的关键字，摘要。 Signed-off-by: aubrey <zhujun@ranko.cn>

--- components/cms/views/articleinfo.php
+++ components/cms/views/articleinfo.php
@@ -3,9 +3,7 @@
 $id = $_REQUEST['id'];
 $article = $context->getPostInfo($id);
 
-$context->setMeta($article['obj']['title'], "无锡，蓝科，无锡蓝科，蓝科创想",
-    "无锡蓝科创想科技有限公司，主要从事软件研发的软件企业。创新，责任，为服务的企业创造价值，是我们的初衷；合作共赢，是我们的愿景！
-");
+$context->setMeta($article['obj']['title'], $article['obj']['keywords'],$article['obj']['summary']);
 
 
 ?>
