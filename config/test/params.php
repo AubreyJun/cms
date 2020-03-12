5f036bf8e20c69d719d77b825c1b2cb619dd98a0
aubrey <zhujun@ranko.cn>
2020-03-12 17:07:14 +0800

    Signed-off-by: aubrey <zhujun@ranko.cn>

---
+++ config/test/params.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'demo'=>'true',
+    'themeids'=>array(
+        'dev.biz.cms.cn'=>'guanggao_1'
+    ),
+    'debug'=>array(
+        'enable'=>true,
+        'name'=>'ranko',
+        'password'=>'2329*(23sww'
+
+    )
+];
