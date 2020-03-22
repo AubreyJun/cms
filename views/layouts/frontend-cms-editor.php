3d3d913fc0010d543f154b83e717df1a44ff4a66
Aubrey <zhujun@ranko.cn>
2020-03-22 22:32:47 +0800

    Signed-off-by: Aubrey <zhujun@ranko.cn>

--- views/layouts/frontend-cms-editor.php
+++ views/layouts/frontend-cms-editor.php
@@ -19,6 +19,7 @@ $widgetObject = json_decode($widgetjson, true);
     <link rel="stylesheet" href="themes/cms/css/magnific-popup.css" type="text/css"/>
 
     <link rel="stylesheet" href="themes/cms/css/responsive.css" type="text/css"/>
+    <link rel="stylesheet" href="themes/cms/css/custom.css" type="text/css"/>
     <meta name="viewport" content="width=device-width, initial-scale=1"/>
 
     <!--    title,keywords,description-->
@@ -32,8 +33,10 @@ $widgetObject = json_decode($widgetjson, true);
 	============================================= -->
 <div id="wrapper" class="clearfix">
 
+    <div class="editor_content" id="editor_"></div>
+
     <?php
-    if(isset($widgetObject['header'])){
+    if (isset($widgetObject['header'])) {
         foreach ($widgetObject['header'] as $widget) {
             echo $this->context->widget($widget['widgetId']);
         }
@@ -43,7 +46,7 @@ $widgetObject = json_decode($widgetjson, true);
     <?php echo $content ?>
 
     <?php
-    if(isset($widgetObject['footer'])){
+    if (isset($widgetObject['footer'])) {
         foreach ($widgetObject['footer'] as $widget) {
             echo $this->context->widget($widget['widgetId']);
         }
@@ -66,28 +69,97 @@ $widgetObject = json_decode($widgetjson, true);
 <script src="themes/cms/js/functions.js"></script>
 
 
-<div id='editor_slider'>
-    helloword
-    <i class="i-rounded i-small icon-cogs" id="editor_trigger" style="margin: 0px !important;border-radius: 0px;position: absolute;right: -30px;" ></i>
+<div id='editor_slider' style="z-index: 2000;background: lightgrey;" class="border-right">
+    <i class="i-rounded i-middle icon-cogs" id="editor_trigger"
+       style="margin: 0px !important;border-radius: 0px;position: absolute;right: -53px;"></i>
+    <div id="editor_plugin">
+        <nav class="navbar navbar-light bg-light justify-content-between">
+            <a class="navbar-brand">组件</a>
+        </nav>
+
+        <div id="editor_plugin_inner" class="editor_plugin_inner" style="padding: 20px;z-index: 2001;overflow-y: scroll;overflow-x: hidden;">
+            <div class="toggle toggle-bg">
+                <div class="togglet" style="padding-left: 16px;">基础</div>
+                <div class="togglec border-bottom" style="padding: 0px;background: #FFFFFF;">
+                    <div class="container">
+                        <div class="row ">
+                            <div class="col-lg-6 mb-3 mt-3"><button type="button" class="btn btn-outline-secondary btn-block">Default</button></div>
+                            <div class="col-lg-6 mb-3 mt-3"><button type="button" class="btn btn-outline-secondary btn-block">Default</button></div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-6 mb-3"><button type="button" class="btn btn-outline-secondary btn-block">Default</button></div>
+                            <div class="col-lg-6 mb-3"><button type="button" class="btn btn-outline-secondary btn-block">Default</button></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="toggle toggle-bg">
+                <div class="togglet" style="padding-left: 16px;">专业</div>
+                <div class="togglec border-bottom" style="padding: 0px;">Lorem ipsum dolor sit amet, consectetur
+                    adipisicing elit. Assumenda, dolorum, vero ipsum molestiae minima odio quo voluptate illum excepturi
+                    quam cum voluptates doloribus quae nisi tempore necessitatibus dolores ducimus enim libero eaque
+                    explicabo suscipit animi at quaerat aliquid ex expedita perspiciatis? Saepe, aperiam, nam unde quas
+                    beatae vero vitae nulla.
+                </div>
+            </div>
+
+            <div class="toggle toggle-bg">
+                <div class="togglet" style="padding-left: 16px;">常规</div>
+                <div class="togglec border-bottom" style="padding: 0px;">Lorem ipsum dolor sit amet, consectetur
+                    adipisicing elit. Assumenda, dolorum, vero ipsum molestiae minima odio quo voluptate illum excepturi
+                    quam cum voluptates doloribus quae nisi tempore necessitatibus dolores ducimus enim libero eaque
+                    explicabo suscipit animi at quaerat aliquid ex expedita perspiciatis? Saepe, aperiam, nam unde quas
+                    beatae vero vitae nulla.
+                </div>
+            </div>
+
+            <div class="toggle toggle-bg">
+                <div class="togglet" style="padding-left: 16px;">网站</div>
+                <div class="togglec border-bottom" style="padding: 0px;">Lorem ipsum dolor sit amet, consectetur
+                    adipisicing elit. Assumenda, dolorum, vero ipsum molestiae minima odio quo voluptate illum excepturi
+                    quam cum voluptates doloribus quae nisi tempore necessitatibus dolores ducimus enim libero eaque
+                    explicabo suscipit animi at quaerat aliquid ex expedita perspiciatis? Saepe, aperiam, nam unde quas
+                    beatae vero vitae nulla.
+                </div>
+            </div>
+            <div class="toggle toggle-bg">
+                <div class="togglet" style="padding-left: 16px;">网站33333</div>
+                <div class="togglec border-bottom" style="padding: 0px;">Lorem ipsum dolor sit amet, consectetur
+                    adipisicing elit. Assumenda, dolorum, vero ipsum molestiae minima odio quo voluptate illum excepturi
+                    quam cum voluptates doloribus quae nisi tempore necessitatibus dolores ducimus enim libero eaque
+                    explicabo suscipit animi at quaerat aliquid ex expedita perspiciatis? Saepe, aperiam, nam unde quas
+                    beatae vero vitae nulla.
+                </div>
+            </div>
+
+        </div>
+    </div>
+
 </div>
 
 <script src="static/frontend/lib/slidereveal/jquery.slidereveal.min.js"></script>
 <script>
+    var nicescroll = null;
     $(function () {
+        var height = $(document).height();
+        $("#editor_plugin_inner").css("height", height - 40);
         $("#editor_slider").slideReveal({
             trigger: $("#editor_trigger"),
-            shown: function(slider, trigger){
+            width: '20%',
+            shown: function (slider, trigger) {
+                $("body").css("margin-left", "20%");
+            },
+            hidden: function (slider, trigger) {
+                $("body").css("margin-left", "0px");
+            },
+            show: function (slider, trigger) {
 
             },
-            hidden: function(slider, trigger){
+            hide: function (slider, trigger) {
 
             },
-            show: function(slider, trigger){
-
-            },
-            hide: function(slider, trigger){
-
-            }
+            push: false
         });
     });
 </script>
