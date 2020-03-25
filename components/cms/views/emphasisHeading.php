87f10ef513af955ea47300cbf662426bd3428999
Aubrey <zhujun@ranko.cn>
2020-03-25 19:15:05 +0800

    删除多余的代码。 Signed-off-by: Aubrey <zhujun@ranko.cn>

--- components/cms/views/emphasisHeading.php
+++
@@ -1,8 +0,0 @@
-<?php
-$properties = $fragment['properties'];
-$head = json_decode($properties,true);
-?>
-<div class="heading-block center">
-    <h3><?php echo $title['pvalue']; ?></h3>
-    <span><?php echo $description['pvalue']; ?></span>
-</div>
--- components/cms/views/headContent.php
+++
@@ -1,8 +0,0 @@
-<?php
-$properties = $fragment['properties'];
-$headContent = json_decode($properties,true);
-?>
-<div class="heading-block">
-    <h3><?php echo $headContent['title']; ?></h3>
-</div>
-<?php echo $headContent['content']; ?>
