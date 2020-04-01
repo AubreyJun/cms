4d9c84d7116fc6ead7352bd84beaed1d98e459f7
Aubrey <zhujun@ranko.cn>
2020-04-01 18:08:58 +0800

    模板更新 Signed-off-by: Aubrey <zhujun@ranko.cn>

--- views/fragment/46/125.php
+++ views/fragment/46/125.php
@@ -3,7 +3,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 <!-- Bootstrap CSS -->
-<link rel="stylesheet" href="themes/complex/bootswatch/cosomo/bootstrap.css" >
+<link rel="stylesheet" href="themes/complex/bootswatch/cosmo/bootstrap.css" >
 
 <title><?php echo $meta_title; ?></title>
 
