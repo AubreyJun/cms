393754c74785efd3689c16af8c058b7384d17eeb
Aubrey <zhujun@ranko.cn>
2020-04-09 23:35:21 +0800

    Signed-off-by: Aubrey <zhujun@ranko.cn>

---
+++ web/static/backend/lib/jodit/connector/Application.php
@@ -0,0 +1,29 @@
+<?php
+
+use Jodit\Application;
+
+class JoditRestApplication extends Application
+{
+	/**
+	 * @example Jodit connector in Joomla
+	 * ```php
+	 * function checkPermissions() {
+	 *      $user = JFactory::getUser();
+	 *      if (!$user->id) {
+	 *           trigger_error('You are not authorized!', E_USER_WARNING);
+	 *      }
+	 * }
+	 * ```
+	 */
+	function checkAuthentication()
+	{
+		return true;
+		// Rewrite this code for your system
+		// if (empty($_SESSION['filebrowser'])) {
+		//     trigger_error('You do not have permission to view this directory, E_USER_WARNING);
+		// }
+
+		// read more https://github.com/xdan/jodit-connectors#configuration
+		throw new \ErrorException('You need override `checkAuthentication` method in file `Application.php` more https://github.com/xdan/jodit-connectors#configuration', 501);
+	}
+}
